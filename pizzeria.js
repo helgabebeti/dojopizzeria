1c8b9f3caff35e691e68059e5b8921ebb42cc9fa
helgabebeti <93710228+helgabebeti@users.noreply.github.com>
2023-08-03 17:58:19 +0200

    Update pizzeria.js

--- pizzeria.js
+++ pizzeria.js
@@ -16,3 +16,28 @@ var p3= pizzaOven ("thin and crispy", "tomato", ["sausage","extra cheese"], ["ba
 console.log (p3);
 var p4 = pizzaOven("thin and crispy","marinara",["bacon","mozzarela","peperoni"],["pesto","olives"]);
 console.log(p4);
+
+function randomRange(max,min){
+  return Math.floor( Math.random( max - min))+ min;
+}
+function randomPick(arr){
+    var i = Math.floor(arr.length * Math.random());
+    return arr[i];
+}
+
+function randomPizza() {
+    var pizza = {};
+    pizza.crustType = randomPick(crustTypes);
+    pizza.sauceType = randomPick(sauceTypes);
+    pizza.cheeses = [];
+    pizza.toppings = [];
+    for(var i=0; i<randomRange(5, 1); i++) {
+        pizza.cheeses.push(randomPick(cheeses));
+    }
+    for(var i=0; i<randomRange(4, 0); i++) {
+        pizza.toppings.push(randomPick(toppings));
+    }
+    return pizza;
+}
+
+console.log(randomPizza());
